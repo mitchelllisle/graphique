04b2d209d5025da32a622c37ca7bdd6deeff0bf2
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-03-24 23:36:12 +1100

    testing circle

--- setup.py
+++ setup.py
@@ -10,30 +10,8 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-# circleci.py version
-VERSION = "1.1.1"
-
-def readme():
-    """print long description"""
-    with open('README.rst') as f:
-        return f.read()
-
-
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
-
 setup(name='graphique',
-      version=VERSION,
+      version='0.13',
       description='A python package for making plotly simple to get up and running',
       url='http://github.com/mitchelllisle/graphique',
       author='Mitchell Lisle',
@@ -41,8 +19,5 @@ setup(name='graphique',
       license='MIT',
       packages=['graphique'],
       zip_safe=False,
-      python_requires='>=3',
-      cmdclass={
-        'verify': VerifyVersionCommand,
-        }
+      python_requires='>=3'
 )
